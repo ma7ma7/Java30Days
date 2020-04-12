514aa157728c159a862fa5ce5b47df07528216b9
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-04-12 15:38:58 +0100

    initial commit

---
+++ 30Days/src/SweetProgramme.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SweetProgramme {
+	public static void main(String[] args) {
+		
+		System.out.println("Enter your name : ");
+//		Scanner sn = new Scanner(System.in);
+//		String s = sn.nextLine();
+//		
+//		if (s.equals("mahmoud")) {
+//			
+//			System.out.println("YOur welcome");
+//		}
+//		
+		int i = 0;
+		
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+//		sn.close();
+	}
+}
